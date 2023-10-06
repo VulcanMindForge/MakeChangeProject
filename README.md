0137ef53e765a7d41f67b0085d44d644eddfa540
VulcanMindForge <jastuart@live.com>
2023-10-06 14:46:37 -0700

    initialized

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MakeChangeProject
+
+Initializing...
+Initalizing..
---
+++ src/MakeChange/Register.java
@@ -0,0 +1,14 @@
+/*
+Synopsis:
+Author:
+Version: 
+ */
+package MakeChange;
+
+public class Register {
+
+	public static void main(String[] args) {
+
+	}
+
+}
