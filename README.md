b970e6a83e93647ba4ddcbb054b7ad048e68b09b
VulcanMindForge <jastuart@live.com>
2023-10-06 17:56:46 -0700

    Rough draft of program and progress update

--- README.md
+++ README.md
@@ -13,10 +13,22 @@
  <p align="left"> Calculate the amount of change returned to a customer based on the purchase price and the amount tendered. Notify the attendant how many of each type of currency ($100, $20, $10, $5, $1, .25c, .10c, .05c, .01c) is needed to make the change for the customer.</p>
 
 <h3 align="Left">Technologies used:</h3>
-<p align="left">Overall I am learning and using Eclipse IDE to program in Java 1.8. Prior to this assignment we covered: Variables and Constants, Methods, Screen Output and Keyboard Input, Expressions, If/Else, Switch, Whil loops, For Loops, Numerals, and Data Types. I plan to implement as many of these systems as make sense to get a better grasp of their uses and best practices.</p>
+<p align="left">Overall I am learning and using Eclipse IDE to program in Java 1.8. Prior to this assignment we covered: Variables and Constants, Methods, Screen Output and Keyboard Input, Expressions, If/Else, Switch, While loops, For Loops, Cast Numerals. I plan to implement as many of these systems as make sense to get a better grasp of their uses and best practices.</p>
 
 <h3 align="Left">Current progress:</h3>
-<p align="left"> Initialized class. </p> 
+<p align="left">
+	<ol> 
+		<li>Created method to do the change logic: If/Else, Expressions, Cast Numerals</li>
+		<li>Declared variables and tested method call: Methods, Variables and constants</li>
+		<li>Initiated scanner and got input from user: Screen output and Keyboard input</li>
+	</ol>
+</p> 
 
 <h3 align="Left">Next Step:</h3>
-<p align="left"> Plan out variables, methods, and general structure of program. </p> 
+<p align="left"> 
+	<ol> 
+		<li>Write out logic to discard denominations that are not needed: for loop</li>
+		<li>Create transaction menu and finish methods: switch</li>
+		<li>Create loop to keep performing transactions until finished: while loops</li>
+	</ol>
+</p> 
--- src/MakeChange/Register.java
+++ src/MakeChange/Register.java
@@ -1,7 +1,8 @@
 /*
-Synopsis:
-Author:
-Version: 
+Synopsis: Calculate the amount of change returned to a customer based on the purchase price and the amount tendered. 
+Notify the attendant how many of each type of currency ($100, $20, $10, $5, $1, .25c, .10c, .05c, .01c) is needed to make the change for the customer.
+Author: Jacob Stuart
+Version: 1.0
  */
 package MakeChange;
 
@@ -10,12 +11,114 @@ import java.util.Scanner;
 public class Register {
 
 	public static void main(String[] args) {
+		//Declare Scanner and other variables for Main
 		Scanner kb = new Scanner(System.in);
+		double amountPayed, amountChange, amountOwed;
+		boolean customer = true;
+		
+		//begin loop by opening drawer and initializing transaction menu.
+		cashDrawer();
+		
+		//TODO While loop with menu to determine function needed and allow shut down
+		System.out.println("Please enter the cost of the item: ");
+		amountOwed = kb.nextDouble();
+
+		System.out.println("Please enter the amount the customer gave you for the item: ");
+		amountPayed = kb.nextDouble();
+		
+		amountChange = amountPayed - amountOwed;
+		
+		//TODO handle transaction
+		makeChange(amountChange);
+		
 		
 		kb.close();
 	}
 	
-	public static void makeChange() {
-		
+	//function: Menu for deciding purchase/refund/side work
+	
+	public static void cashDrawer() {
+		System.out.println();
+		System.out.print("         ______________________________  \n"
+						+"        /       /       /      /      /  \n"
+						+"       /       /       /      /      /   \n"
+						+"      / [$20] / [$10] / [$5] / [$1] /    \n"
+						+"     /       /       /      /      /     \n"
+						+"    /_______/_______/______/______/      \n"
+						+"   /       /       /      /      /       \n"
+						+"  / [25¢] / [10¢] / [5¢] / [1¢] /        \n"
+						+" /_______/_______/______/______/         \n");
+		System.out.println();
 	}
+	
+	public static String transactionType(Scanner kb) {
+		String menuChoice = "";
+		
+		
+		
+		return menuChoice;
+	}
+
+	//function: determine and print proper amounts of change for customer.
+	public static void makeChange(double change) {
+		double denPennyVal = 0.01;
+		double denNickleVal = 0.05;
+		double denDimeVal = 0.15;
+		double denQuarterVal = 0.25;
+		double den1DolVal = 0.05;
+		double den2DolVal = 2.00;
+		double den5DolVal = 5.00;
+		double den10DolVal = 10.00;
+		double den20DolVal = 20.00;
+		int num20 = 0;
+		int num10 = 0;
+		int num5 = 0;
+		int num2 = 0;
+		int num1 = 0;
+		int numQuart = 0;
+		int numDime = 0;
+		int numNickle = 0;
+		int numPenny = 0;
+		
+
+			if (change / den20DolVal >= 0) {
+				num20 = (int) (change / 20.00);
+				change = change % 20.00;
+			}
+			if (change / den10DolVal >= 0) {
+				num10 = (int) (change / 10.00);
+				change = change % 10.00;
+			}
+			if (change / den5DolVal >= 0) {
+				num5 = (int) (change / 5.00);
+				change = change % 5.00;
+			}
+			if (change / den2DolVal >= 0) {
+				num2 = (int) (change / den2DolVal);
+				change = change % 2.00;
+			}
+			if (change / den1DolVal >= 0) {
+				num1 = (int) (change / den1DolVal);
+				change = change % 1.00;
+			}
+			if (change / denQuarterVal >= 0) {
+				numQuart = (int) (change / 0.25);
+				change = change % denQuarterVal;
+			}
+			if (change / denDimeVal >= 0) {
+				numDime = (int) (change / denDimeVal);
+				change = change % 0.10;
+			}
+			if (change / denNickleVal >= 0) {
+				numNickle = (int) (change / denNickleVal);
+				change = change % 0.05;
+			}
+			if (change / denPennyVal >= 0) {
+				numPenny = (int) ((change + 0.005) / denPennyVal);
+			}
+			
+			System.out.println(numPenny + " Pennies " + numNickle + " Nickles " + numDime + " Dimes " + numQuart + " Quarters " + num1 + " 1$ bills " + num2 + " 2$ bills " + num5 + " 5$ bills " + num10 + " 10$ bills " + num20 + " 20$ bills ");
+
+	}
+	
 }
