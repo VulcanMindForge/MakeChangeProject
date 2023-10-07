21e7a001e80b6c6f1a1e33ba5be4060d7b615268
VulcanMindForge <jastuart@live.com>
2023-10-07 11:54:24 -0700

    Version 1.0 working

--- README.md
+++ README.md
@@ -3,32 +3,40 @@
 <h1 align="center">Hi 👋, I'm Jacob Stuart</h1>
 <h3 align="center">Programming student with Skill Distillery</h3>
 
-- 🔭 I’m currently working on [MakeChangeProject](https://github.com/VulcanMindForge/MakeChangeProject)
+- I’m currently working on [MakeChangeProject](https://github.com/VulcanMindForge/MakeChangeProject)
 
-- 🌱 I’m currently learning **Java**
+- I’m currently learning **Java**
 
-- 👨‍💻 All of my projects are available at [https://github.com/VulcanMindForge](https://github.com/VulcanMindForge)
+- All of my projects are available at [https://github.com/VulcanMindForge](https://github.com/VulcanMindForge)
 
 <h3 align="left">Purpose of this project:</h3>
  <p align="left"> Calculate the amount of change returned to a customer based on the purchase price and the amount tendered. Notify the attendant how many of each type of currency ($100, $20, $10, $5, $1, .25c, .10c, .05c, .01c) is needed to make the change for the customer.</p>
+
+<h3 align="left">How to use:</h3>
+ <p align="left"> The program will greet you and ask if you are ready to take a customer. If you choose no, the program will say farewell and close. If you say yes you will be prompted to enter the cost of the item. After entering the cost you will be prompted to enter the amount recieved fromt the customer. If the customer gives exact change we thank them, if they don't give enough we explain it is not enough and request the remainder. If the customer give us more than enough we provide the amount of change they should recieve and break it down to the correct number of largest denominations to equal their change amount.</p>
+ <p align="left"> You can modify the methods to convert to other monetary systems. Change the switch method to the proper names and amounts. Change the loop method to match the number of denominations set up in the switch method. Change the numMonies method so that the lowest denomination gets rounded properly.</p>
+
 
 <h3 align="Left">Technologies used:</h3>
 <p align="left">Overall I am learning and using Eclipse IDE to program in Java 1.8. Prior to this assignment we covered: Variables and Constants, Methods, Screen Output and Keyboard Input, Expressions, If/Else, Switch, While loops, For Loops, Cast Numerals. I plan to implement as many of these systems as make sense to get a better grasp of their uses and best practices.</p>
 
 <h3 align="Left">Current progress:</h3>
-<p align="left">
+<p align="left"> Finished with first version. Made some changes to the way the system worked after realizing I was repeating code. 
 	<ol> 
-		<li>Created method to do the change logic: If/Else, Expressions, Cast Numerals</li>
-		<li>Declared variables and tested method call: Methods, Variables and constants</li>
-		<li>Initiated scanner and got input from user: Screen output and Keyboard input</li>
+		<li>Created switch method to do denomination calls and assign variables: Switch, call method with return</li>
+		<li>Created loop that loops over every case in the switch method to check all denominations: For Loop, Method call, pass through variables, modify variable each loop.</li>
+		<li>Created logic method numMonies that takes checks the given denomination and prints the amount needed to make change: Numerals, Casting, Method with param and return, If/Else, Sysout.</li>
+		<li>Created multiple art methods including a greeting, farewell, and money drawer.: void method, sysout</li>
+		<li>In Main, called greeting, set an infinite while loop with exit condition that calls farewell and ends loop. Loop calls for user input to determine if transaction needed, set scanner to take input from user, set nested if statements for makeing change, exact change, not enough payment. : PrintF, if/else, while.
+		<li>Set variables and used scope across all methods.</li>
 	</ol>
 </p> 
 
 <h3 align="Left">Next Step:</h3>
 <p align="left"> 
 	<ol> 
-		<li>Write out logic to discard denominations that are not needed: for loop</li>
-		<li>Create transaction menu and finish methods: switch</li>
-		<li>Create loop to keep performing transactions until finished: while loops</li>
+		<li>Have users test program and return suggestions for features</li>
+		<li>Arrange for transaction where customer didn't provide enough money to continue until they have fully paid.</li>
+		<li>Test with other currency systems</li>
 	</ol>
 </p> 
--- src/MakeChange/Register.java
+++ src/MakeChange/Register.java
@@ -11,114 +11,186 @@ import java.util.Scanner;
 public class Register {
 
 	public static void main(String[] args) {
-		//Declare Scanner and other variables for Main
+		// Declare Scanner and other variables for Main
 		Scanner kb = new Scanner(System.in);
-		double amountPayed, amountChange, amountOwed;
-		boolean customer = true;
+		double amountPayed, amountChange, itemCost;
+		int nextCustomer;
+		boolean isCustomer = true;
 		
-		//begin loop by opening drawer and initializing transaction menu.
-		cashDrawer();
-		
-		//TODO While loop with menu to determine function needed and allow shut down
-		System.out.println("Please enter the cost of the item: ");
-		amountOwed = kb.nextDouble();
+		// Greet user
+		greeting();
 
-		System.out.println("Please enter the amount the customer gave you for the item: ");
-		amountPayed = kb.nextDouble();
-		
-		amountChange = amountPayed - amountOwed;
-		
-		//TODO handle transaction
-		makeChange(amountChange);
-		
-		
+		// Prompt user if customers are present, if so loop through customer transactions, if not say goodbye.
+		while (isCustomer) {
+			
+			System.out.println();
+			System.out.println("Are you ready to take the next customer?");
+			System.out.println("\t1: Yes");
+			System.out.println("\t2: No");
+			nextCustomer = kb.nextInt();
+			kb.nextLine();
+			
+			//Perform customer transaction, if change is needed call change method, if exact change thank customer, if not enough let customer know remaining cost.
+			if (nextCustomer == 1) {
+				System.out.println("Please enter the cost of the item being purchased: ");
+				itemCost = kb.nextDouble();
+				kb.nextLine();
+
+				System.out.println("Please enter the amount the customer gave you for the item: ");
+				amountPayed = kb.nextDouble();
+				kb.nextLine();
+
+				amountChange = amountPayed - itemCost;
+
+				if (amountChange > 0) {
+					cashDrawer();
+
+					System.out.printf("The customer's change is %.2f\n", amountChange);
+
+					changeLoop(amountChange);
+				} else if (amountChange == 0) {
+					System.out.println("Thank you for providing exact change");
+				} else {
+					itemCost = itemCost - amountPayed;
+					System.out.printf("I am sorry, this is not enough. You still owe: %.2f\n", itemCost);
+				}
+			} else {
+				isCustomer = false;
+				farewell();
+			}
+		}
+
 		kb.close();
 	}
-	
-	//function: Menu for deciding purchase/refund/side work
-	
+
+	// function: Iterate through all denominations in the switch. Modify initial i value to match number of cases in changeSwitch
+	public static void changeLoop(double change) {
+		for (int i = 8; i > 0; i--) {
+			change = changeSwitch(i, change);
+		}
+	}
+
+	// function: switch to set denominations. Modify to whichever monetary system is needed.
+	public static double changeSwitch(int switchNum, double change) {
+		double denomValue = 0.0;
+		String denomination = "";
+
+		switch (switchNum) {
+		case 1:
+			denomination = "Pennies";
+			denomValue = 0.01;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		case 2:
+			denomination = "Nickles";
+			denomValue = 0.05;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		case 3:
+			denomination = "Dimes";
+			denomValue = 0.10;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		case 4:
+			denomination = "Quarters";
+			denomValue = 0.25;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		case 5:
+			denomination = "$1 bills";
+			denomValue = 1.00;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		case 6:
+			denomination = "$5 bills";
+			denomValue = 5.00;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		case 7:
+			denomination = "$10 bills";
+			denomValue = 10.00;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		case 8:
+			denomination = "$20 bills";
+			denomValue = 20.00;
+			change = numMonies(denomination, denomValue, change);
+			return change;
+		default:
+			System.out.println("Something has gone horribly wrong!");
+			return change;
+		}
+	}
+
+	// function: calculate and print proper amounts of change for customer. Modify "Pennies" string to match lowest denomination and round appropriately
+	public static double numMonies(String denomName, double denomination, double amount) {
+		double amountLeft = 0;
+		double preNumMonies = 0;
+		int numMonies = 0;
+
+		if (amount / denomination >= 0) {
+			preNumMonies = amount / denomination;
+			amountLeft = amount % denomination;
+		}
+
+		if ("Pennies".equals(denomName)) {
+			preNumMonies = preNumMonies + 0.01;
+		}
+
+		numMonies = (int) preNumMonies;
+
+		if (numMonies > 0) {
+			System.out.println(numMonies + " " + denomName);
+		}
+
+		return amountLeft;
+	}
+
+	// fun image
 	public static void cashDrawer() {
 		System.out.println();
-		System.out.print("         ______________________________  \n"
-						+"        /       /       /      /      /  \n"
-						+"       /       /       /      /      /   \n"
-						+"      / [$20] / [$10] / [$5] / [$1] /    \n"
-						+"     /       /       /      /      /     \n"
-						+"    /_______/_______/______/______/      \n"
-						+"   /       /       /      /      /       \n"
-						+"  / [25¢] / [10¢] / [5¢] / [1¢] /        \n"
-						+" /_______/_______/______/______/         \n");
+		System.out.print ("         ______________________________  \n" 
+						+ "        /       /       /      /      /  \n"
+						+ "       /       /       /      /      /   \n" 
+						+ "      / [$20] / [$10] / [$5] / [$1] /    \n"
+						+ "     /       /       /      /      /     \n" 
+						+ "    /_______/_______/______/______/      \n"
+						+ "   /       /       /      /      /       \n" 
+						+ "  / [25¢] / [10¢] / [5¢] / [1¢] /        \n"
+						+ " /_______/_______/______/______/         \n");
 		System.out.println();
 	}
-	
-	public static String transactionType(Scanner kb) {
-		String menuChoice = "";
-		
-		
-		
-		return menuChoice;
+
+	// Greeting image
+	public static void greeting() {
+		System.out.println();
+		System.out.print ("        _____   _____   _____  ___   \n" 
+						+ "       /    /  /    /  /    / /   \\ \n"
+						+ "      /    /  /    /  /    / /    /  \n" 
+						+ "     /  __   /    /  /    / /    /   \n" 
+						+ "    /____/  /____/  /____/ /____/    \n"
+						+ "        ___                          \n" 
+						+ "       /   \\   /\\   \\   /         \n" 
+						+ "      /    /  /__\\   \\ /           \n" 
+						+ "     /    /  /    \\   /             \n" 
+						+ "    /____/  /      \\ /              \n");
+		System.out.println();
 	}
 
-	//function: determine and print proper amounts of change for customer.
-	public static void makeChange(double change) {
-		double denPennyVal = 0.01;
-		double denNickleVal = 0.05;
-		double denDimeVal = 0.15;
-		double denQuarterVal = 0.25;
-		double den1DolVal = 0.05;
-		double den2DolVal = 2.00;
-		double den5DolVal = 5.00;
-		double den10DolVal = 10.00;
-		double den20DolVal = 20.00;
-		int num20 = 0;
-		int num10 = 0;
-		int num5 = 0;
-		int num2 = 0;
-		int num1 = 0;
-		int numQuart = 0;
-		int numDime = 0;
-		int numNickle = 0;
-		int numPenny = 0;
-		
-
-			if (change / den20DolVal >= 0) {
-				num20 = (int) (change / 20.00);
-				change = change % 20.00;
-			}
-			if (change / den10DolVal >= 0) {
-				num10 = (int) (change / 10.00);
-				change = change % 10.00;
-			}
-			if (change / den5DolVal >= 0) {
-				num5 = (int) (change / 5.00);
-				change = change % 5.00;
-			}
-			if (change / den2DolVal >= 0) {
-				num2 = (int) (change / den2DolVal);
-				change = change % 2.00;
-			}
-			if (change / den1DolVal >= 0) {
-				num1 = (int) (change / den1DolVal);
-				change = change % 1.00;
-			}
-			if (change / denQuarterVal >= 0) {
-				numQuart = (int) (change / 0.25);
-				change = change % denQuarterVal;
-			}
-			if (change / denDimeVal >= 0) {
-				numDime = (int) (change / denDimeVal);
-				change = change % 0.10;
-			}
-			if (change / denNickleVal >= 0) {
-				numNickle = (int) (change / denNickleVal);
-				change = change % 0.05;
-			}
-			if (change / denPennyVal >= 0) {
-				numPenny = (int) ((change + 0.005) / denPennyVal);
-			}
-			
-			System.out.println(numPenny + " Pennies " + numNickle + " Nickles " + numDime + " Dimes " + numQuart + " Quarters " + num1 + " 1$ bills " + num2 + " 2$ bills " + num5 + " 5$ bills " + num10 + " 10$ bills " + num20 + " 20$ bills ");
-
+	// farewell image
+	public static void farewell() {
+		System.out.println();
+		System.out.print ("        _____   _____   _____  ___   \n" 
+						+ "       /       /    /  /    / /   \\ \n"
+						+ "      /       /    /  /    / /    /  \n" 
+						+ "     /  __   /    /  /    / /    /   \n" 
+						+ "    /____/  /____/  /____/ /____/    \n"
+						+ "        ___                _____     \n" 
+						+ "       /   \\   \\   /     /        \n" 
+						+ "      /____/    \\ /     /____      \n" 
+						+ "     /    /      /     /            \n" 
+						+ "    /____/      /     /_____        \n");
+		System.out.println();
 	}
-	
+
 }
